239818eb4bf0408f390d1c852d877cd86aa18fe4
anastasiiasher <nastyasher@me.com>
2021-09-16 15:49:07 +0200

    deleted due to time management

--- src/script.js
+++
@@ -1,7 +0,0 @@
-function darkmode(event) {
-    event.preventDefault();
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-}
-document.querySelector("button#darkmode")=btndm;
-btndm.addEventListener("click", darkmode);

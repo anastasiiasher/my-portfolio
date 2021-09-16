c121eaad3d7928b4b69a5911d6cbcc853772d31f
anastasiiasher <nastyasher@me.com>
2021-09-16 15:33:16 +0200

    just curious

---
+++ src/script.js
@@ -0,0 +1,6 @@
+function darkmode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+let darkmode = document.querySelector(#darkmode);
+darkmode.addEventListener( "click", darkmode)
